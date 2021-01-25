f3f9ab0f3dab29c98d62ea5dc59789a06a961eee
Ahmon Dancy <adancy@wikimedia.org>
2021-01-25 08:54:56 -0800

    use db init job, modifed l10n setup

---
+++ mw/init-db.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+function wait_for_db {
+    local max_attempts=60
+
+    echo "$(date): Waiting for DB server $DB_SERVER to become available to $DB_USER"
+
+    for ((attempt=0; attempt<$max_attempts; attempt++)); do
+        if mysql -h "$DB_SERVER" -u "$DB_USER" -p"$DB_PASS" </dev/null; then
+            echo "$(date): DB is available"
+            return
+        else
+            sleep 1
+        fi
+    done
+    # If we reached here, we had no luck
+    echo "$(date): DB not available.  Giving up"
+    exit 1
+}
+
+# Operations below require SSL connectivity to etcd, so make sure the list of CA certs
+# is up-to-date.
+update-ca-certificates
+
+wait_for_db
+
+cd /srv/mediawiki
+
+for wiki in enwiki; do
+    # install.php won't do anything if it finds LocalSettings.php
+    rm -f php/LocalSettings.php
+    echo $wiki: Performing basic db setup
+    # FIXME: Factor our admin username and password
+    php php/maintenance/install.php $wiki admin --pass secret123123123123 \
+        --dbserver "$DB_SERVER" \
+        --dbname $wiki \
+        --dbuser "$DB_USER" \
+        --dbpass "$DB_PASS"
+
+    # Now that the basics are set up, use update.php with usual
+    # LocalSettings.php in place.  This will create additional tables
+    # that are required by enabled extensions.
+    cat <<EOF > php/LocalSettings.php
+<?php
+require __DIR__ . "/../wmf-config/CommonSettings.php";
+EOF
+    echo $wiki: Update db
+    mwscript maintenance/update.php --wiki=$wiki --quick
+done
---
+++ mw/init-l10n.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+# FIXME:
+# * hardcoded /srv/mediawiki/wikiversions-dev.json
+
+function _call_rebuildLocalisationCache {
+    local wikidb=$1
+    local cache_dir=$2
+    local lang=$3
+    local force=${4:-}
+
+    if [ "$lang" ]; then
+         lang="--lang $lang"
+    fi
+    if [ "$force" ]; then
+        force="--force"
+    fi
+
+    # FIXME: This is a kubernetes anti-pattern.  We should heed the
+    # container cpu limit if there is one.  Also keep in mind that using
+    # more cpus means more memory use.
+    local ncpus=$(getconf _NPROCESSORS_ONLN)
+    
+    sudo -u www-data \
+         /usr/local/bin/mwscript rebuildLocalisationCache.php \
+         --wiki=$wikidb \
+         --store-class=LCStoreCDB \
+         --threads=$ncpus \
+         --no-clear-message-blob-store \
+         $lang \
+         $force
+}
+
+# xref mediawiki/tools/scap/scap/tasks.py:update_localization_cache()
+function update_localization_cache {
+    local version=$1 # (without php- prefix)
+    local wikidb=$2
+
+    echo "update_localization_cache for $version (using $wikidb)"
+    
+    local extension_messages=/srv/mediawiki/wmf-config/ExtensionMessages-$version.php
+    touch $extension_messages
+
+    #local cache_dir=/srv/mediawiki/php-$version/cache/l10n
+    local cache_dir=/tmp/l10n-cache/$version
+    local force_rebuild=
+
+    echo cache_dir: $cache_dir
+    sudo -u www-data mkdir -p $cache_dir
+
+    if [ ! -f $cache_dir/l10n_cache-en.cdb ]; then
+        # mergeMessageFileList.php needs a l10n file
+        echo Bootstrapping l10n cache for $version
+        _call_rebuildLocalisationCache $wikidb $cache_dir en
+        # Force subsequent cache rebuild to overwrite bootstrap version
+        force_rebuild=true
+    fi
+
+    echo Updating $extension_messages
+    local ext_list=/srv/mediawiki/wmf-config/extension-list
+
+    sudo -u www-data /usr/local/bin/mwscript mergeMessageFileList.php --wiki=$wikidb --list-file=$ext_list --output=/tmp/new-ext-messages --quiet
+    mv /tmp/new-ext-messages $extension_messages
+
+    # Rebuild all the CDB files for each language
+    echo Updating LocalisationCache for $version
+
+    _call_rebuildLocalisationCache $wikidb $cache_dir "" $force_rebuild
+}
+
+
+function unique_wikiversions {
+    sed -e '/^{/d' -e '/^}/d' -e 's/,$//' -e 's/"//g' -e 's/php-//' /srv/mediawiki/wikiversions-dev.json | awk '{print $2}' | sort -u
+}
+
+function representative_db_for_version {
+    local version=$1
+    
+    fgrep php-$version /srv/mediawiki/wikiversions-dev.json  | head -1 | sed -e 's/"//g' -e 's/://' | awk '{print $1}'
+}
+
+update-ca-certificates
+
+for version in $(unique_wikiversions); do
+    update_localization_cache $version $(representative_db_for_version $version)
+done
